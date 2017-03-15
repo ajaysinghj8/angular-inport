8060d2504736965148308658632464eb63eb3cd0
Plutazoo <meajaysingh@hotmail.com>
2017-03-15 20:14:54 +0530

    added inview-container and inview-item

--- src/inview.module.ts
+++ src/inview.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { InviewDirective } from './inview.directive';
+import { InviewContainerDirective } from './inview-container.directive';
+import { InviewItemDirective } from './inview-item.directive';
 import { ScrollObservable } from './utils/scroll-observable';
 import { WindowRuler } from './utils/viewport-ruler';
 @NgModule({
   imports: [],
-  declarations: [InviewDirective],
-  exports: [InviewDirective],
+  declarations: [InviewDirective, InviewContainerDirective, InviewItemDirective],
+  exports: [InviewDirective, InviewContainerDirective, InviewItemDirective],
   providers: [ScrollObservable, WindowRuler]
 })
 export class Ng2InviewModule { }
