5fc896af8147f08402ca97e9260b9d7bd7da45bf
Plutazoo <meajaysingh@hotmail.com>
2017-03-15 19:03:01 +0530

    added inview-container

---
+++ src/inview-container.directive.ts
@@ -0,0 +1,105 @@
+import { Directive, ContentChildren, QueryList, OnInit, OnDestroy, AfterViewInit, Input, Output, EventEmitter, ElementRef, NgZone } from '@angular/core';
+import { Observable, Subscription } from 'rxjs/Rx';
+
+import { InviewItemDirective } from './inview-item.directive';
+import { ScrollObservable } from './utils/scroll-observable';
+import { WindowRuler } from './utils/viewport-ruler';
+import { OffsetResolverFactory } from './utils/offset-resolver';
+import { PositionResolver } from './utils/position-resolver';
+import { ElementBoundingPositions } from './utils/models';
+// allmost same configuration as child
+// child will not have inview property? to trigger changes
+// will use scroll on this or window
+// will check wheather the child is in view port
+// will start checking from last inview child and with direction of scroll until a child is not visible
+// can return all inview children
+// or best match case
+// if container is used then first check if container itself is in the viewport of the window.
+// then only the futher calculation should take place
+
+@Directive({
+  selector: '[inview-container]'
+})
+export class InviewContainerDirective implements OnInit, OnDestroy, AfterViewInit {
+  private _scrollSuscription: Subscription;
+  private _throttleType: string = 'debounce';
+  private _offset: Array<number> = [0, 0, 0, 0];
+  private _viewPortOffset: Array<number> = [0, 0, 0, 0];
+  private _throttle: number = 0;
+  private _scrollWindow: boolean = true;
+  private _data: any;
+
+
+  @Input()
+  set offset(offset: Array<number> | number) {
+    this._offset = OffsetResolverFactory.create(offset).normalizeOffset();
+  }
+  @Input()
+  set viewPortOffset(offset: Array<number> | number) {
+    this._viewPortOffset = OffsetResolverFactory.create(offset).normalizeOffset();
+  }
+  @Input()
+  set throttle(throttle: number) {
+    this._throttle = throttle;
+  }
+  @Input()
+  set scrollWindow(sw: boolean) {
+    this._scrollWindow = !!sw;
+  }
+  @Input()
+  set data(_d: any) {
+    this._data = _d;
+  }
+
+  @Output() inview: EventEmitter<any> = new EventEmitter();
+  @ContentChildren(InviewItemDirective) private _inviewChildren: QueryList<InviewItemDirective>;
+
+  constructor(
+    private _element: ElementRef,
+    private _scrollObservable: ScrollObservable,
+    private _windowRuler: WindowRuler,
+    private _zone: NgZone
+  ) { }
+
+  ngOnInit() { }
+  ngAfterViewInit() {
+    this._scrollSuscription = this._scrollObservable.scrollObservableFor(window)
+    [this._throttleType](() => Observable.timer(this._throttle))
+      .filter(() => true)
+      .mergeMap((event: any) => Observable.of(this._getViewPortRuler()))
+      .subscribe((containersBounds: ElementBoundingPositions) => this.handleOnScroll(containersBounds));
+
+  }
+  private _getViewPortRuler() {
+    return this._scrollWindow ? this._windowRuler.getWindowViewPortRuler() : PositionResolver.getBoundingClientRect(this._element.nativeElement);
+  }
+  ngOnDestroy() {
+    if (this._scrollSuscription) {
+      this._scrollSuscription.unsubscribe();
+    }
+  }
+
+  handleOnScroll(containersBounds: ElementBoundingPositions) {
+    // check of scroll up or down
+    // Note:: check all children from parent if it is in view or not
+    // for cache of less iterations start from the last visible  item then based on scroll up and down check list futher
+    let viewPortOffsetRect = PositionResolver.offsetRect(containersBounds, this._viewPortOffset);
+
+    if (this._inviewChildren) {
+      let visibleChildren: Array<any> = this._inviewChildren.toArray().filter((child: InviewItemDirective) => {
+        let elementOffsetRect = PositionResolver.offsetRect(child.getELementRect(), this._offset);
+        return child.isVisible() && PositionResolver.intersectRect(elementOffsetRect, viewPortOffsetRect);
+      }).map((child: InviewItemDirective) =>  child.getData());
+      this._zone.run(() => this.inview.emit(visibleChildren));
+    }
+  }
+
+}
+
+
+//  inview-container -> inview-item ->
+
+//  scrollWindow =  true -> will test it against the window scroll event with container.
+//  scrollWindow = false -> means we need to attach scroll event on this container.
+
+// inview ->  directly used against the window.
